af2d68f83b5dedd95775691b24191460b26ed864
0xb1b1 <voxelflee@protonmail.com>
2023-12-17 01:19:43 +0300

    util(backend,vk-bot): introduce message_type to Message schema

--- backend/src/cjsc_backend/http/schemas/message.py
+++ backend/src/cjsc_backend/http/schemas/message.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 from pydantic import BaseModel, Field
 from datetime import datetime
+from enum import Enum
 
 from cjsc_backend.models.message import \
     MessagePlatform
+
+
+class MessageRequestType(str, Enum):
+    CHAT_BOT = "chat_bot"
+    EVENTS = "events"
+    WEATHER = "weather"
+    TRAFFIC_JAM = "traffic_jam"
+    MINI_APP = "mini_app"
+    SOCIAL_NETWORKS = "social_networks"
+    TRASH = "trash"
 
 
 class MessageSchema(BaseModel):
@@ -27,4 +38,9 @@ class MessageSchema(BaseModel):
 
     response_text: str | None = Field(
         description="Response message text. None if of Request type",
+    )
+
+    request_type: MessageRequestType | None = Field(
+        default=None,
+        description="Request type as detected by ML",
     )
